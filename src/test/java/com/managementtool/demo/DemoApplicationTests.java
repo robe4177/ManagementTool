ab011c807b72ca032a35b1b2efdc0265e241a6d6
Robi <robe4177@stud.kea.dk>
2020-11-30 14:36:00 +0100

    First Commit

---
+++ src/test/java/com/managementtool/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managementtool.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
