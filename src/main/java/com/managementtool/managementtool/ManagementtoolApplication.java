b32cdecec3d5cbc8b8814c207a8f03a55fec75f6
abdulahiroble <abdulahi_2610@hotmail.com>
2020-11-30 11:57:20 +0100

    Initialize spring boot

---
+++ src/main/java/com/managementtool/managementtool/ManagementtoolApplication.java
@@ -0,0 +1,13 @@
+package com.managementtool.managementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managementtool/managementtool/ManagementtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managementtool.managementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
